1efa877fd5e74bfd4d462742f4485a764d6068bc
Sriram <sriramsb@thoughtworks.com>
2018-04-08 22:00:41 +0700

    [Sriram] Initial commit for puppeteer

---
+++ google_search.spec.js
@@ -0,0 +1,28 @@
+const assert = require('assert')
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+
+before(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 10,
+    });
+    page = await browser.newPage()
+})
+
+describe('Check Google Homepage in debug mode', () => {
+  it('has title "Google"', async () => {
+    await page.goto('https://google.com', { waitUntil: 'networkidle0' })
+    await page.type("input[name=q]", "puppeteer github");
+    await page.click("input[name=btnK]");
+    await page.waitForSelector("#resultStats");
+    const mainTitleText = await page.$eval(".r a", el => el.textContent);
+    assert.equal(mainTitleText,"GitHub - GoogleChrome/puppeteer: Headless Chrome Node API");
+  }).timeout(10000)
+})
+
+after(async () => {
+  await browser.close()
+})
---
+++ google_search_headless.spec.js
@@ -0,0 +1,27 @@
+const assert = require('assert')
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+
+before(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+    });
+    page = await browser.newPage()
+})
+
+describe('Check Google Homepage in headless mode', () => {
+  it('has title "Google"', async () => {
+    await page.goto('https://google.com', { waitUntil: 'networkidle0' })
+    await page.type("input[name=q]", "puppeteer github");
+    await page.click("input[name=btnK]");
+    await page.waitForSelector("#resultStats");
+    const mainTitleText = await page.$eval(".r a", el => el.textContent);
+    assert.equal(mainTitleText,"GitHub - GoogleChrome/puppeteer: Headless Chrome Node API");
+  }).timeout(10000)
+})
+
+after(async () => {
+  await browser.close()
+})
