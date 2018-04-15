f1d9c631ee9836dcf54c1867be9cd82359c21339
Sriram <sriramsb@thoughtworks.com>
2018-04-15 15:23:51 +0700

    [Sriram] Added more performance metrics reading

--- google_search_pageMetrics.spec.js
+++ google_search_pageMetrics.spec.js
@@ -7,19 +7,24 @@ let page
 before(async () => {
     browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      slowMo: 80,
+      headless: false,
+      slowMo: 100,
     });
     page = await browser.newPage()
 })
 
-describe('Check Google Homepage in headless mode', () => {
+describe('Check Google performance metrics', () => {
   it('has title "Google"', async () => {
     await page.goto('https://google.com', { waitUntil: 'networkidle0' })
     await page.type("input[name=q]", "puppeteer github");
     await page.click("input[name=btnK]");
     await page.waitForSelector("#resultStats");
     const metrics = await page.metrics();
-    console.log(JSON.stringify(metrics))
+    console.log("metrics" + JSON.stringify(metrics))
+    const performanceTiming = JSON.parse(
+      await page.evaluate(() => JSON.stringify(window.performance.timing))
+    );
+    console.log("something : "+performanceTiming)
     const mainTitleText = await page.$eval(".r a", el => el.textContent);
     assert.equal(mainTitleText,"GitHub - GoogleChrome/puppeteer: Headless Chrome Node API");
     await page.screenshot({path: 'screenshot.png'})
